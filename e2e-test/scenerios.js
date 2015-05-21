2284770861593c94c7c39404043a3127effaf5b6
andela-eashikodi <emeka.ashikodi@andela.co>
2015-05-21 02:48:31 +0100

    add spec test

--- e2e-test/scenerios.js
+++ e2e-test/scenerios.js
@@ -1,55 +1,69 @@
 'use strict';
 
 describe('Enroll app', function(){
+  browser.get('index.html');
 
-  beforeEach(function(){
-    browser.get('index.html');
-  });
-  
-
-  it('should redirect to loin page', function(){
+  it('should redirect to login page', function(){
     expect(browser.getLocationAbsUrl()).toMatch(/login/);
   });
 
-  it('should accept a valid login and perform admin roles', function(){
-    var username = element(by.model('login.username'));
-    var password = element(by.model('login.password'));
-    var findStudent = element(by.model('find.lastname'));
-    var newFirstname = element(by.model('student.firstname'));
-    var newLastname = element(by.model('student.lastname'));
-    var newGender = element(by.model('student.gender'));
-    var newState = element(by.model('student.state'));
-    var newNumber = element(by.model('student.phone'));
-    var newDob = element(by.model('student.dob'));
-    
-    var noResult = element.all(by.id('noResult'));
-    var loginButton = element.all(by.id('login'));
-    var viewStudents = element.all(by.id('viewStudents'));
-    var createStudent = element.all(by.id('createStudent'));
-    var newReg = element.all(by.id('newReg'));
-    var studentList = element.all(by.repeater('student in filtered = ( list | filter: find)'));
+  describe('Ctrl', function(){
+    it('should perform valid login', function(){
+      var username = element(by.model('login.username'));
+      var password = element(by.model('login.password'));
+      var loginButton = element.all(by.id('login'));
 
-    username.sendKeys('tom');
-    password.sendKeys('tom');
-    loginButton.click();
-    expect(browser.getLocationAbsUrl()).toMatch(/admin/);
+      username.sendKeys('tom');
+      password.sendKeys('tom');
 
-    createStudent.click();
-    newFirstname.sendKeys('Cole');
-    newLastname.sendKeys('Adam');
-    newGender.sendKeys('Male');
-    newState.sendKeys('Adamawa');
-    newNumber.sendKeys('234992992');
-    newDob.sendKeys('11/11/1990');
-    newReg.click();
+      loginButton.click();
+      expect(browser.getLocationAbsUrl()).toMatch(/admin/);
 
-    viewStudents.click();
-    findStudent.sendKeys('unregistered student');
-    expect(studentList.count()).toBe(0);
-  
+    });
+
+    it('should successfully edit a user', function(){
+      var editFirstname = element(by.model('user.firstname'));
+      var editUser = element.all(by.id('editUser'));
+      var editButton = element.all(by.id('edit'));
+
+      editUser.click();
+      editFirstname.sendKeys('-s');
+      editButton.click();
+    });
+
+    it('should be able to register student', function(){
+      browser.get('/#/admin/create');
+      var newFirstname = element(by.model('student.firstname'));
+      var newLastname = element(by.model('student.lastname'));
+      var newGender = element(by.model('student.gender'));
+      var newState = element(by.model('student.state'));
+      var newNumber = element(by.model('student.phone'));
+      var newDob = element(by.model('student.dob'));
+      var createStudent = element.all(by.id('createStudent'));
+      var newReg = element.all(by.id('newReg'));
+
+      createStudent.click();
+      newFirstname.sendKeys('Cole');
+      newLastname.sendKeys('Adam');
+      newGender.sendKeys('Male');
+      newState.sendKeys('Adamawa');
+      newNumber.sendKeys('234992992');
+      newDob.sendKeys('11/11/1990');
+      newReg.click();
+      expect(browser.getLocationAbsUrl()).toMatch(/list/);
+    });
+
+    it('should perform search on a student', function(){
+      browser.get('/#/admin/list');
+      var studentList = element.all(by.repeater('student in filtered = ( list | filter: find)'));
+      var findStudent = element(by.model('find.lastname'));
+      findStudent.sendKeys('unregistered-student');
+      expect(studentList.count()).toBe(0);
+    });
   });
 
   it('should reject invalid login', function(){
+    browser.get('index.html');
     var username = element(by.model('login.username'));
     var password = element(by.model('login.password'));
     var loginButton = element.all(by.id('login'));
--- unit-test/test.js
+++ unit-test/test.js
@@ -2,16 +2,19 @@
 
 describe('Enroll App', function(){
   var AdminScope, LoginScope, StudentScope;
+  var http, $httpBackend, service;
   var AdminCtrl, LoginCtrl, StudentCtrl;
   beforeEach(function(){
     module('Enroll');
-    inject(function($controller, $rootScope){
+    inject(function($controller, $rootScope, _$httpBackend_, $http, ApiService){
+      service = ApiService;
+      $httpBackend = _$httpBackend_;
       AdminScope = $rootScope.$new();
       LoginScope = $rootScope.$new();
       StudentScope = $rootScope.$new();
-      AdminCtrl = $controller('AdminCtrl', {$scope:AdminScope});
-      LoginCtrl = $controller('LoginCtrl', {$scope:AdminScope});
-      StudentCtrl = $controller('StudentCtrl', {$scope:AdminScope});
+      AdminCtrl = $controller('AdminCtrl', {$scope:AdminScope, $http: http});
+      LoginCtrl = $controller('LoginCtrl', {$scope:LoginScope, $http: http});
+      StudentCtrl = $controller('StudentCtrl', {$scope:StudentScope, $http: http});
     });
   });
 
@@ -31,6 +34,25 @@ describe('Enroll App', function(){
 
     it('should remove user profile', function(){
       expect(AdminScope.removeUser).toBeDefined();
+    });
+  });
+
+  describe('Api Service', function(){
+    it('should be defined', function(){
+     expect(service.getStudents()).toBeDefined();
+    });
+
+    it('should successfully make http GET students request', function(){
+      var students;
+      $httpBackend.whenGET("/api/v1/students").respond([{
+        id: 1,
+        name: "john"
+      }]);
+      service.getStudents().then(function(res){
+        students = res;
+        $httpBackend.flush();
+        expect(students instanceof Array).toBeTruthy();
+      });
     });
   });
 
